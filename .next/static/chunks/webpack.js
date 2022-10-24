1e3b6be0b5d4cdfc1df40fdb77ffe21726289be6
Uyen Cao <caophuonguyen132002@gmail.com>
2022-10-24 20:03:27 +0700

    update footer in constant folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5a3e7693731d33c5"; }
+/******/ 		__webpack_require__.h = function() { return "f65c4665af1a3d50"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
