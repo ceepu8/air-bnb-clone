73985992090ed9066a79fe4fbac1d00e14b5e4a2
Uyen Cao <caophuonguyen132002@gmail.com>
2023-01-05 21:07:06 +0700

    add feature for date update modal

--- src/views/Book/DateAndGuest/Date.tsx
+++ src/views/Book/DateAndGuest/Date.tsx
@@ -1,29 +1,54 @@
 import { Button, Calendar, Modal } from "@/components"
 import { useFlag } from "@/hooks"
+import classNames from "classnames"
 import dayjs from "dayjs"
 import { useRouter } from "next/router"
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import { DateRange } from "react-day-picker"
 
-const convertedDate = (from: string | string[], to: string | string[]): DateRange => {
+const convertedDate = (
+  from: string | string[] | undefined,
+  to: string | string[] | undefined
+): any => {
   const newDate = {
-    from: dayjs(from.toString()).toDate(),
-    to: dayjs(to.toString()).toDate(),
+    from: from && dayjs(from.toString()).toDate(),
+    to: to && dayjs(to.toString()).toDate(),
   }
   return newDate
 }
 
 const Date = () => {
-  const { query } = useRouter()
-  const { from = "", to = "", numberNights } = query
-  const date: DateRange = useMemo(() => convertedDate(from, to), [])
+  const router = useRouter()
+  console.log(router.query)
 
-  const [currentDate, setDate] = useState<DateRange | undefined>(convertedDate(from, to))
-  const handleChangeDate = (date: DateRange | undefined) => setDate(date)
-  const [isOpen, setTrue, setFalse] = useFlag(false)
+  const [date, setDateQuery] = useState(router.query)
+  const { from, to, numberNights } = date || {}
+
+  const [isOpen, onOpen, onClose] = useFlag(false)
+
+  const handleChangeDate = (_date: DateRange | undefined) => {
+    const { from: _from, to: _to } = _date || {}
+    console.log(_date)
+
+    setDateQuery((prev) => ({
+      ...prev,
+      from: _from && dayjs(_from).format("YYYY-MM-DD"),
+      to: _to && dayjs(_to).format("YYYY-MM-DD"),
+    }))
+  }
+
+  const handleUpdateDate = () => {
+    if (from && to) {
+      router.push({
+        pathname: `/book/${router.query.id}`,
+        query: date,
+      })
+      onClose()
+    }
+  }
 
   useEffect(() => {
-    setDate(date)
+    setDateQuery(router.query)
   }, [isOpen])
 
   return (
@@ -31,17 +56,18 @@ const Date = () => {
       <div>
         <p className="font-medium text-black-gray">Ngày</p>
         <p className="mt-1 font-light text-black-gray">
-          {dayjs(date.from).format("DD/MM/YYYY")} đến {dayjs(date.to).format("DD/MM/YYYY")}
+          {dayjs(router.query.from?.toString()).format("DD/MM/YYYY")} đến{" "}
+          {dayjs(router.query.to?.toString()).format("DD/MM/YYYY")}
         </p>
       </div>
-      <Button className="cursor-pointer font-medium underline" text="black" onClick={setTrue}>
+      <Button className="cursor-pointer font-medium underline" text="black" onClick={onOpen}>
         Chỉnh sửa
       </Button>
-      <Modal isOpen={isOpen} title="Calendar" onClose={setFalse}>
+      <Modal isOpen={isOpen} title="Calendar" onClose={onClose}>
         <div className="mb-4">
           <div className="px-6">
             <Calendar
-              date={currentDate}
+              date={convertedDate(from, to)}
               numberNights={Number(numberNights)}
               onSelect={handleChangeDate}
             />
@@ -50,7 +76,14 @@ const Date = () => {
             <Button text="black" className="mr-4 underline">
               Xoá ngày
             </Button>
-            <Button variant="black" className="rounded-lg py-2 px-5">
+            <Button
+              variant="black"
+              disabled={!from || !to}
+              className={classNames("rounded-lg py-2 px-5", {
+                "cursor-not-allowed opacity-50": !from || !to,
+              })}
+              onClick={handleUpdateDate}
+            >
               Lưu
             </Button>
           </div>
