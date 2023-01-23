af82d0d06468bd65eca711f812d34bac68d9a968
tcdtist <chithanh.tist@gmail.com>
2023-01-23 22:51:09 +0700

    update navigations map object

--- src/components/navigation/HeaderNavigate.tsx
+++ src/components/navigation/HeaderNavigate.tsx
@@ -1,24 +1,32 @@
 import { NavLink } from "../base"
 
 const HeaderNav = () => {
+  const navigations = [
+    {
+      label: "Chỗ ở",
+      slug: "/",
+    },
+    {
+      label: "Trải nghiệm",
+      slug: "/",
+    },
+    {
+      label: "Trải nghiệm trực tuyến",
+      slug: "/",
+    },
+  ]
+
   return (
     <nav>
       <ul className="mb-0 flex items-center justify-center pt-4">
-        <li className="m-2">
-          <NavLink disabled href="/">
-            <p className="text-md relative pb-2 font-light">Chỗ ở</p>
-          </NavLink>
-        </li>
-        <li className="m-2">
-          <NavLink disabled href="/">
-            <p className="text-md relative pb-2 font-light">Trải nghiệm</p>
-          </NavLink>
-        </li>
-        <li className="m-2">
-          <NavLink disabled href="/">
-            <p className="text-md relative pb-2 font-light">Trải nghiệm trực tuyến</p>
-          </NavLink>
-        </li>
+        {navigations.map((item) => (
+          <li key={item.label} className="m-2">
+            // TODO: disabled
+            <NavLink href={item.slug} disabled={!!item.slug}>
+              <span className="text-md relative pb-2 font-light">{item.label}</span>
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   )
