84d53eda9d139ea9f38d21f17e93948c2adf62b3
Uyen Cao <caophuonguyen132002@gmail.com>
2023-02-01 16:08:56 +0700

    update social component as reviewed

--- src/components/base/Social/index.tsx
+++ src/components/base/Social/index.tsx
@@ -5,12 +5,14 @@ import React from "react"
 
 export const Social = ({ href = "", name = "", Icon }: SocialInterface) => {
   return (
-    <Link href={href} target="_blank">
-      <a className="mt-4 flex cursor-pointer items-center rounded-md border-[1px] border-solid border-black-gray py-3 px-5 hover:bg-very-light-gray">
-        <Icon className={classNames("h-4 w-4")} />
-        <p className="flex-1 text-center text-sm font-medium text-black-gray">
-          Tiếp tục với {name}
-        </p>
+    <Link href={href}>
+      <a rel="noopener noreferrer" target="_blank">
+        <div className="mt-4 flex cursor-pointer items-center rounded-md border-[1px] border-solid border-black-gray py-3 px-5 hover:bg-very-light-gray">
+          <Icon className={classNames("h-4 w-4")} />
+          <p className="flex-1 text-center text-sm font-medium text-black-gray">
+            Tiếp tục với {name}
+          </p>
+        </div>
       </a>
     </Link>
   )
