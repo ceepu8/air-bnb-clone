52b999cb4e4472a3b67a7d68ea22d9531e599a85
Uyen Cao <caophuonguyen132002@gmail.com>
2023-02-03 12:41:27 +0700

    add alert after register success

--- src/constants/Message.ts
+++ src/constants/Message.ts
@@ -6,6 +6,8 @@ export const MESSAGE = {
   DELETE_BOOKING_CONFIRM: "Bạn có chắc chắn muốn huỷ đặt phòng?",
   LOGIN_SUCCESS: "Đăng nhập thành công!",
   LOGIN_FAILED: "Đăng nhập thất bại!",
-  REGISTER_SUCCESS: "Đăng ký thành công!",
-  REGISTER_FAILED: "Đăng ký thất bại!",
+  REGISTER: {
+    SUCCESS: "Đăng ký thành công!",
+    FAILED: "Đăng ký thất bại!",
+  },
 }
