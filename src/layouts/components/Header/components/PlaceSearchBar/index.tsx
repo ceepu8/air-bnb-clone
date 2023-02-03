2fd4c601a5baef87c573f389d1b4cb0230db280d
Uyen Cao <caophuonguyen132002@gmail.com>
2023-02-03 14:11:32 +0700

    handle close searchbar (by scroll event)

--- src/layouts/components/Header/components/PlaceSearchBar/index.tsx
+++ src/layouts/components/Header/components/PlaceSearchBar/index.tsx
@@ -1,7 +1,19 @@
+import { SET_SHRINK } from "@/store/actions"
+import { useDispatch } from "react-redux"
 import SelectionBox from "./SelectionBox"
 import SelectionBoxExtend from "./SelectionBoxExtend"
+import { useEventListener } from "@/hooks"
+import { useCallback } from "react"
 
 const PlaceSearchBar = ({}: {}) => {
+  const dispatch = useDispatch()
+
+  const handler = useCallback(() => {
+    dispatch(SET_SHRINK())
+  }, [])
+
+  useEventListener("scroll", handler)
+
   return (
     <>
       <SelectionBoxExtend />
